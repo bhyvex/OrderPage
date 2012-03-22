966f24babdb65cf546b419bd88519c3b977833c3
Ivan Kustera <ivan@atomia.com>
2012-03-22 15:45:14 +0100

    Modified Original files for Web.config and Atomia.Web.Plugin.HostingProducts.dll.config with changes made for resellers. Version updated to 12.5.2.0.
    Change-Id: Id8e7034d7c85bd43eab6fcf29a9e362adfb1ddeb

--- Atomia/Web/Solution/SolutionInfo.cs
+++ Atomia/Web/Solution/SolutionInfo.cs
@@ -5,5 +5,5 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("12.5.0.0")]
-[assembly: AssemblyFileVersion("12.5.0.0")]
+[assembly: AssemblyVersion("12.5.2.0")]
+[assembly: AssemblyFileVersion("12.5.2.0")]

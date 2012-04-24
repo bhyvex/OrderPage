a60452d3c66308a00db5e595c442d04231527350
Dusan Milenkovic <dusan@atomia.com>
2012-04-24 09:45:26 +0200

    Updated API reference assemblies. Updated version number.
    Change-Id: I1dff87d82399c6b8ec0ca85e4edffb9c34d5261e

--- Atomia/Web/Solution/SolutionInfo.cs
+++ Atomia/Web/Solution/SolutionInfo.cs
@@ -5,5 +5,5 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("12.5.4.0")]
-[assembly: AssemblyFileVersion("12.5.4.0")]
+[assembly: AssemblyVersion("12.5.5.0")]
+[assembly: AssemblyFileVersion("12.5.5.0")]

15e774e87d66ae27ca70e23739eb4e788a7211bf
ivanr <ivanr@atomia.com>
2012-08-13 12:00:58 +0200

    Updated external assemblies. Version is changed from 12.7.0.0 to 12.7.2.0. There is no changes in code, new release is made only to update api references.
    Change-Id: I9268931152687e0ad3ef9bd6ca48d2883abbfeb1

--- Atomia/Web/Solution/SolutionInfo.cs
+++ Atomia/Web/Solution/SolutionInfo.cs
@@ -5,5 +5,5 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("12.7.0.0")]
-[assembly: AssemblyFileVersion("12.7.0.0")]
+[assembly: AssemblyVersion("12.7.2.0")]
+[assembly: AssemblyFileVersion("12.7.2.0")]

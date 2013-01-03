640a7762a7f9a8f8ca28be5712e19ff0eb3e23dd
Igor Jocic <igor@atomia.com>
2013-01-03 15:21:18 +0100

    Updated referenced dlls. Version updated to 13.1.0.0
    Change-Id: I6c79e57e6002a9894915978066c42537d1353e45

--- Atomia/Web/Solution/SolutionInfo.cs
+++ Atomia/Web/Solution/SolutionInfo.cs
@@ -5,5 +5,5 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("12.9.1.0")]
-[assembly: AssemblyFileVersion("12.9.1.0")]
+[assembly: AssemblyVersion("13.1.0.0")]
+[assembly: AssemblyFileVersion("13.1.0.0")]
